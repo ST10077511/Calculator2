2a48969cd138c44b0d03ae8b3b023ae5c12f817c
Bongani Lekwane <ST10077511@vcconnect.edu.za>
2024-04-09 18:41:35 +0200

    first commit

---
+++ app/src/main/java/com/st10077511/calculator2/MainActivity.kt
@@ -0,0 +1,190 @@
+package com.st10077511.calculator2
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+
+class MainActivity : AppCompatActivity() {
+
+    var etNum1 : EditText? = null
+    var etNum2 : EditText? = null
+    var tvDis : EditText? = null
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        etNum1 = findViewById(R.id.etNum1)
+        etNum2 = findViewById(R.id.etNum2)
+        tvDis = findViewById(R.id.tvDisp)
+
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        btnAdd.setOnClickListener{add()}
+
+        val btnSub = findViewById<Button>(R.id.btnSub)
+        btnSub.setOnClickListener{sub()}
+
+        val btnMul = findViewById<Button>(R.id.btnMul)
+        btnMul.setOnClickListener{mul()}
+
+        val btnDiv = findViewById<Button>(R.id.btnDiv)
+        btnDiv.setOnClickListener{div()}
+
+        val btnSqr = findViewById<Button>(R.id.btnSqr)
+        btnSqr.setOnClickListener{sqr()}
+
+        val btnPow = findViewById<Button>(R.id.btnPow)
+        btnPow.setOnClickListener{pow()}
+    }
+
+    private fun add(){}
+    private fun sub(){}
+    private fun mul(){}
+    private fun div(){}
+    private fun sqr(){}
+    private fun pow(){
+        var intNum1 = etNum1?.toString()!!.toInt()
+        var intNum2 = etNum2?.toString()?.toInt()
+        var intAnswer : Int = 1
+
+        val input1 = etNum1?.text?.toString()
+        val input2 = etNum2?.text?.toString()
+
+        if (input1.isNullOrEmpty() || input2.isNullOrEmpty()){
+            tvDis?.text = "Please enter valid numbers."
+            return
+        }
+
+        val intNum1 = input1.toInt()
+        val intNum2 = input2.toInt()
+        var intAnswer = 1
+
+        for(i in 1..intNum2){
+            intAnswer *= intAnswer * intNum1
+        }
+        tvDis?.text = intAnswer.toString()
+
+    private fun add(){
+        val input1 = etNum1?.text?.toString()
+        val input2 = etNum2?.text?.toString()
+
+        if (input1.isNullOrEmpty() || input2.isNullOrEmpty()){
+            tvDis?.text = "Please enter the valid numbers."
+            return
+        }
+
+        val intNum1 = input1.toInt()
+        val intNum2 = input2.toInt()
+        var intAnswer = 0
+
+        intAnswer = intNum1 + intNum2
+
+        tvDis?.text = intAnswer.toString()
+    }
+
+    private fun sub(){
+        val input1 = etNum1?.text?.toString()
+        val input2 = etNum2?.text?.toString()
+
+        if (input1.isNullOrEmpty() || input2.isNullOrEmpty()){
+            tvDis?.text = "Please enter the valid numbers."
+            return
+        }
+
+        val intNum1 = input1.toInt()
+        val intNum2 = input2.toInt()
+        var intAnswer = 0
+
+        intAnswer = intNum1 - intNum2
+
+        tvDis?.text = intAnswer.toString()
+    }
+
+    private fun mul(){
+        val input1 = etNum1?.text?.toString()
+        val input2 = etNum2?.text?.toString()
+
+        if (input1.isNullOrEmpty() || input2.isNullOrEmpty()){
+            tvDis?.text = "Please enter the valid numbers."
+            return
+        }
+
+        val intNum1 = input1.toInt()
+        val intNum2 = input2.toInt()
+        var intAnswer = 0
+
+        intAnswer = intNum1 * intNum2
+
+        tvDis?.text = intAnswer.toString()
+
+    }
+
+    private fun div(){
+        val input1 = etNum1?.text?.toString()
+        val input2 = etNum2?.text?.toString()
+
+        if (input1.isNullOrEmpty() || input2.isNullOrEmpty()){
+            tvDis?.text = "Please enter the valid numbers."
+            return
+        }
+
+        val intNum1 = input1.toInt()
+        val intNum2 = input2.toInt()
+        var fltAnswer = 0.0
+
+        if (intNum2 == 0) {
+            tvDis?.text = "Division by zero is not allowed"
+        }else {
+
+            fltAnswer = intNum1 / intNum2
+
+        }
+
+        tvDis?.text = fltAnswer.toString()
+
+    }
+
+    private fun sqrt() {
+        val input1 = etNum1?.text?.toString()
+        //val input2 = etNum2?.text?.toString()
+
+        if (input1.isNullOrEmpty() || input2.isNullOrEmpty()) {
+            tvDis?.text = "Please enter the valid numbers."
+            return
+        }
+
+        val intNum1 = input1.toInt()
+        //val intNum2 = input2.toInt()
+        var intAnswer = 0
+
+        fltAnswer = sqrt(intNum1)
+
+        tvDis?.text = fltAnswer.toString()
+
+    }
+    private fun pow(){
+        val input1 = etNum1?.text?.toString()
+        val input2 = etNum2?.text?.toString()
+
+        if (input1.isNullOrEmpty() || input2.isNullOrEmpty()){
+            //handle empty inputs (optional)
+            tvDis?.text = "Please enter the valid numbers."
+            return
+        }
+
+        val intNum1 = input1.toInt()
+        val intNum2 = input2.toInt()
+        var intAnswer = 0
+
+        intAnswer = intNum1 ^ intNum2
+
+        tvDis?.text = intAnswer.toString()
+
+    }    }
+
+
+
